657896b2e2120e840164aa935b6b964044748e45
João Marcos <joao.marcos@buser.com.br>
2021-01-21 16:53:16 -0300

    Componentiza a pág de análise

---
+++ frontend/components/lista_analise.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="caixa-de-fora">
+    <div class="flexbox-analise">
+        <card_analysis v-for="req in reqs_analysis" :key="req.id" :req = req></card_analysis>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import card_analysis from '~/components/card_analysis.vue'
+export default {
+    components: {
+        card_analysis
+    },
+    props: ['reqs_analysis']
+}
+</script>
+
+<style scoped>
+.flexbox-analise {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: center;
+}
+.caixa-de-fora {
+  display: flex;
+  justify-content: center;
+  overflow: auto;
+  }
+</style>
